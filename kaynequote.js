4b6d832a3b453aefe10bf7bb8c2cd742efc7f022
Spencer Brown <spencer.brown79@gmail.com>
2020-03-09 18:34:59 -0600

    Spencer - LOTR Quotes

---
+++ kaynequote.js
@@ -0,0 +1,20 @@
+class KayneQuote {
+    constructor(quote) {
+        this.kanyeWisdom = quote;
+    }
+
+    methodOne() {
+        //DO SOMETHING WITH THE QUOTE
+    }
+
+    methodTwo() {
+        //DO SOMETHING WITH THE QUOTE
+    }
+
+    methodThree() {
+        //DO SOMETHING WITH THE QUOTE
+    }
+}
+// MAKE AJAX REQUEST TO GET QUOTE
+// ON COMPLETE, CREATE A NEW OBJECT FROM THE CLASS
+// CALL YOUR THREE METHODS OF YOUR NEW OBJECT
